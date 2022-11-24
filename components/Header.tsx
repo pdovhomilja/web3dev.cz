f1f68018ae3d90232bbc5df9030410f3d9e05eb0
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-11-24 13:38:48 +0100

    Header ready

---
+++ components/Header.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <img
+            className=" object-contain cursor-pointer w-44"
+            src="https://links.papareact.com/yvf"
+            alt="Medium logo"
+          />
+        </Link>
+        <div className="hidden md:inline-flex items-center space-x-5">
+          <h3>O nás</h3>
+          <h3>Kontakt</h3>
+          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
+            Follow
+          </h3>
+        </div>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3 className="">Sign In</h3>
+        <h3 className="border px-4 rounded-full border-green-600">
+          Get Started
+        </h3>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ sanity.js
@@ -0,0 +1,18 @@
+import {
+  createImageUrlBuilder,
+  createCurrentUserHook,
+  createClient,
+} from "next-sanity";
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: "2021-10-21",
+  useCdn: process.env.NODE_ENV === "production",
+};
+
+//SetUp the client for fetching data in the getProps page functions
+export const sanityClient = createClient(config);
+
+//https://www.sanity.io/docs/image-url
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
