256eb89f469ac14d2a785102a1f37ec81557c8c2
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-11-24 19:57:56 +0100

    bugfix

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -45,135 +45,139 @@ function Post({ post }: Props) {
   //console.log(post);
   return (
     <main>
-      <Header />
-      <img
-        className="w-full h-40 object-cover"
-        src={urlFor(post.mainImage).url()!}
-        alt=""
-      />
-      <article className="max-w-3xl mx-auto p-5">
-        <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
-        <h2 className="text-xl font-light text-gray-500 mb-2">
-          {post.description}
-        </h2>
-        <div className="flex items-center space-x-2">
-          <img
-            className="h-10 w-10 rounded-full"
-            src={urlFor(post.author.image).url()!}
-            alt=""
-          />
-          <p className="font-extralight text-sm ">
-            Blog post by:{" "}
-            <span className="text-green-500 pr-1">{post.author.name}</span>-
-            Publish at {new Date(post._createdAt).toLocaleString()}
-          </p>
-        </div>
-        <div className="mt-10">
-          <PortableText
-            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
-            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
-            content={post.body}
-            serializers={{
-              h1: (props: any) => (
-                <h1 className="text-2xl font-bold my-5" {...props} />
-              ),
-              h2: (props: any) => (
-                <h2 className="text-xl font-bold my-5" {...props} />
-              ),
-              li: ({ children }: any) => (
-                <li className="ml-4 list-disc">{children} </li>
-              ),
-              link: ({ href, children }: any) => (
-                <a href={href} className="text-blue-500 hover:underline">
-                  {children}
-                </a>
-              ),
-            }}
-          />
-        </div>
-      </article>
-      <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
-
-      {submitted ? (
-        <div className="flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto">
-          <h3 className="text-3xl font-bold">Děkujeme za Váš komentář</h3>
-          <p>Jakmile ho schválíme zobrazí se pod článkem</p>
-        </div>
-      ) : (
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col p-5 max-w-2xl mx-auto mb-10"
-        >
-          <h3 className="text-sm text-yellow-500">Líbil se Vám článek</h3>
-          <h4 className="text-3xl font-bold">Nechte nám komentář níže!</h4>
-          <hr className="py-3 mt-2" />
-
-          <input
-            {...register("_id")}
-            type="hidden"
-            name="_id"
-            value={post._id}
-          />
-
-          <label className="block mb-5">
-            <span className="text-gray-700">Jméno</span>
-            <input
-              {...register("name", { required: true })}
-              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-              placeholder="John Doe"
-              type="text"
+      <>
+        <Header />
+        <img
+          className="w-full h-40 object-cover"
+          src={urlFor(post.mainImage).url()!}
+          alt=""
+        />
+        <article className="max-w-3xl mx-auto p-5">
+          <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+          <h2 className="text-xl font-light text-gray-500 mb-2">
+            {post.description}
+          </h2>
+          <div className="flex items-center space-x-2">
+            <img
+              className="h-10 w-10 rounded-full"
+              src={urlFor(post.author.image).url()!}
+              alt=""
             />
-          </label>
-          <label className="block mb-5">
-            <span className="text-gray-700">E-mail</span>
-            <input
-              {...register("email", { required: true })}
-              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-              placeholder="John Doe"
-              type="email"
-            />
-          </label>
-          <label className="block mb-5">
-            <span className="text-gray-700">Komentář</span>
-            <textarea
-              {...register("comment", { required: true })}
-              className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-              placeholder="John Doe"
-              rows={8}
-            />
-          </label>
-          <div className="flex  flex-col p-5">
-            {errors.name && (
-              <span className="text-red-500">- Políčko Jméno je povinné</span>
-            )}
-            {errors.email && (
-              <span className="text-red-500">- Políčko E-mail je povinné</span>
-            )}
-            {errors.comment && (
-              <span className="text-red-500">
-                - Políčko Komentář je povinné
-              </span>
-            )}
-          </div>
-          <input
-            type="submit"
-            className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer "
-          />
-        </form>
-      )}
-      {/* Comments */ console.log(post.comments.length, "počet komentářů")}
-      <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2 ">
-        <h3>Komentáře</h3>
-        <hr className="pb-2" />
-        {post.comments.map((comment) => (
-          <div key={comment._id}>
-            <p>
-              <span className="text-yellow-500">{comment.name}: </span>
-              {comment.comment}
+            <p className="font-extralight text-sm ">
+              Blog post by:{" "}
+              <span className="text-green-500 pr-1">{post.author.name}</span>-
+              Publish at {new Date(post._createdAt).toLocaleString()}
             </p>
           </div>
-        ))}
-      </div>
+          <div className="mt-10">
+            <PortableText
+              dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+              projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+              content={post.body}
+              serializers={{
+                h1: (props: any) => (
+                  <h1 className="text-2xl font-bold my-5" {...props} />
+                ),
+                h2: (props: any) => (
+                  <h2 className="text-xl font-bold my-5" {...props} />
+                ),
+                li: ({ children }: any) => (
+                  <li className="ml-4 list-disc">{children} </li>
+                ),
+                link: ({ href, children }: any) => (
+                  <a href={href} className="text-blue-500 hover:underline">
+                    {children}
+                  </a>
+                ),
+              }}
+            />
+          </div>
+        </article>
+        <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
+
+        {submitted ? (
+          <div className="flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto">
+            <h3 className="text-3xl font-bold">Děkujeme za Váš komentář</h3>
+            <p>Jakmile ho schválíme zobrazí se pod článkem</p>
+          </div>
+        ) : (
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col p-5 max-w-2xl mx-auto mb-10"
+          >
+            <h3 className="text-sm text-yellow-500">Líbil se Vám článek</h3>
+            <h4 className="text-3xl font-bold">Nechte nám komentář níže!</h4>
+            <hr className="py-3 mt-2" />
+
+            <input
+              {...register("_id")}
+              type="hidden"
+              name="_id"
+              value={post._id}
+            />
+
+            <label className="block mb-5">
+              <span className="text-gray-700">Jméno</span>
+              <input
+                {...register("name", { required: true })}
+                className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+                placeholder="John Doe"
+                type="text"
+              />
+            </label>
+            <label className="block mb-5">
+              <span className="text-gray-700">E-mail</span>
+              <input
+                {...register("email", { required: true })}
+                className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+                placeholder="John Doe"
+                type="email"
+              />
+            </label>
+            <label className="block mb-5">
+              <span className="text-gray-700">Komentář</span>
+              <textarea
+                {...register("comment", { required: true })}
+                className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+                placeholder="John Doe"
+                rows={8}
+              />
+            </label>
+            <div className="flex  flex-col p-5">
+              {errors.name && (
+                <span className="text-red-500">- Políčko Jméno je povinné</span>
+              )}
+              {errors.email && (
+                <span className="text-red-500">
+                  - Políčko E-mail je povinné
+                </span>
+              )}
+              {errors.comment && (
+                <span className="text-red-500">
+                  - Políčko Komentář je povinné
+                </span>
+              )}
+            </div>
+            <input
+              type="submit"
+              className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer "
+            />
+          </form>
+        )}
+        {/* Comments */ console.log(post.comments.length, "počet komentářů")}
+        <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2 ">
+          <h3>Komentáře</h3>
+          <hr className="pb-2" />
+          {post.comments.map((comment) => (
+            <div key={comment._id}>
+              <p>
+                <span className="text-yellow-500">{comment.name}: </span>
+                {comment.comment}
+              </p>
+            </div>
+          ))}
+        </div>
+      </>
     </main>
   );
 }
