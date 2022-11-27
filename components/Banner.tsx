5d54df390b5406427e7321ffcf7b67137509a5cd
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-11-27 21:41:04 +0100

    bugfix v1.0.1

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,8 +2,8 @@ type Props = {};
 
 function Banner({}: Props) {
   return (
-    <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:p-0">
-      <div className="p-16 space-y-5">
+    <div className="flex flex-col md:flex-row justify-between items-center bg-yellow-400 border-y border-black py-10 lg:p-0">
+      <div className="m-5 md:m-20">
         <h1 className="text-6xl max-w-xl font-serif">
           veškeré <span className="font-bold">know-how</span> o vývoji
           <span className="px-3 underline decoration-black decoration-4">
@@ -11,12 +11,12 @@ function Banner({}: Props) {
           </span>
           na jednom místě
         </h1>
-        <h2>
+        <h2 className="pt-5">
           rady a tipy pro začátečníky ale i pokročilé. Vše popasané tak jak jsme
           sami studovali. Veškeré dosupené know-how na jednom místě.
         </h2>
       </div>
-      <p className="text-8xl font-bold p-20 pr-22">W3D</p>
+      <p className="hidden md:flex text-8xl font-bold p-20 pr-22">W3D</p>
     </div>
   );
 }
