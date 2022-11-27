8cb60bd9f8683dd6c5b8576206c282f7c654ed91
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2022-11-27 16:06:29 +0100

    Add socials

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 
@@ -18,16 +19,30 @@ function Header({}: Props) {
           <h3>
             <Link href="/contact"> Kontakt</Link>
           </h3>
-          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
-            Follow
-          </h3>
         </div>
       </div>
       <div className="flex items-center space-x-5 text-green-600">
-        <h3 className="">Sign In</h3>
-        <h3 className="border px-4 rounded-full border-green-600">
-          Get Started
-        </h3>
+        <div className="mr-2">
+          <SocialIcon
+            className="grayscale opacity-50 hover:opacity-100"
+            style={{ height: 30, width: 30 }}
+            url="https://twitter.com/Web3devCZ"
+          />
+        </div>
+        <div className="mr-2">
+          <SocialIcon
+            className="grayscale opacity-50 hover:opacity-100"
+            style={{ height: 30, width: 30 }}
+            url="https://www.instagram.com/web3dev.cz/"
+          />
+        </div>
+        <div className="mr-2">
+          <SocialIcon
+            className="grayscale opacity-50 hover:opacity-100"
+            style={{ height: 30, width: 30 }}
+            url="https://www.facebook.com/profile.php?id=100088223800566"
+          />
+        </div>
       </div>
     </header>
   );
